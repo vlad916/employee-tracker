5f560bef8395855defa480123d5bfdefb79d9444
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-18 16:37:15 +0930

    Employee Schema created.

---
+++ employee-Schema.sql
@@ -0,0 +1,28 @@
+DROP DATABASE IF exists employeeDB;
+
+CREATE DATABASE employeeDB;
+
+USE employeeDB;
+
+CREATE TABLE department (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+    name varchar(30) NOT NULL
+);
+
+CREATE TABLE roles (
+	id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    title VARCHAR(30),
+    salary DECIMAL NOT NULL,
+    department_id INT NOT NULL,
+    CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id)
+);
+
+CREATE TABLE employee (
+	id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL,
+    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id),
+    manager_id INT,
+    CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES employee(id)
+);
