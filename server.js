59ca6b7ba81c56745f267b95508582e9e00411bb
Angelo Ungos <ungosangelo916@yahoo.com.au>
2020-05-19 22:42:31 +0930

    server.js updated.

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const dbconnection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: 'Xander0530!',
+    password: '',
     database: 'employeeDB'
 })
 
